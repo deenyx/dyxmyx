ea9ef7a6392b55781512e5832800c1210b276f70
Deeman <pocketqueens87@gmail.com>
2026-06-10 01:14:46 -0700

    Remove duplicate header from welcome

--- src/app/(main)/welcome/layout.tsx
+++ src/app/(main)/welcome/layout.tsx
@@ -1,10 +1,8 @@
 import { SiteFooter } from "@/components/site-footer";
-import { Header } from "@/components/header";
 
 export default function WelcomeLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header />
       {children}
       <SiteFooter compact />
     </>
